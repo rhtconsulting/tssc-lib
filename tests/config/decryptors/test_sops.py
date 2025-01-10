c7724326ecfe24905121246ebad40d30cc323479
Jimmy Liang <jimmy.liang_sc@sparksoftcorp.com>
2025-01-10 18:12:22 -0500

    revert changes to test_sops file

--- tests/config/decryptors/test_sops.py
+++ tests/config/decryptors/test_sops.py
@@ -182,11 +182,11 @@ class TestSOPSConfigValueDecryptor(BaseTestCase):
 
         sops_decryptor = SOPS()
 
-        # with self.assertRaisesRegex(
-        #     ValueError,
-        #     r"Given config value \(ConfigValue\(.*\)\) parent source \(None\) " \
-        #     r"is expected to be of type dict or str but is of type: <class 'NoneType'>"
-        # ):
+        with self.assertRaisesRegex(
+            ValueError,
+            r"Given config value \(ConfigValue\(.*\)\) parent source \(None\) " \
+            r"is expected to be of type dict or str but is of type: <class 'NoneType'>"
+        ):
             sops_decryptor.decrypt(config_value)
 
     def test_decrypt_sops_error(self, sops_mock):
